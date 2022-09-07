34735a3eeb3915e50f5f4b8829f0f0f7565dd0fa
Lukhanyo Vakele <lukhanyovakele@gmail.com>
2022-09-07 16:25:37 +0200

    updated user

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 const pgp = require("pg-promise")({})
 const config = {
-    connectionString: process.env.DATABASE_URL || "postgresql://potgres:nimda@localhost:5432/price_plans"
+    connectionString: process.env.DATABASE_URL || "postgresql://postgres:nimda@localhost:5432/price_plans"
 }
 if(process.env.NODE_ENV === "production"){
     config.ssl = {
