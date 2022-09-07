e7a03c4b2c8e0bbf4d512c4a8a1ba87eb3b3a02d
Lukhanyo Vakele <lukhanyovakele@gmail.com>
2022-09-07 13:02:05 +0200

    created database connection

---
+++ db/connection.js
@@ -0,0 +1,11 @@
+const pgp = require("pg-promise")({})
+const config = {
+    connectionString: process.env.DATABASE_URL || "postgresql://potgres:nimda@localhost:5432/price_plans"
+}
+if(process.env.NODE_ENV === "production"){
+    config.ssl = {
+        rejectUnauthorized: false
+    }
+} 
+const db = pgp(config)
+module.exports = db
